6a3ff6be86131a6e542b800957b8e442ac8faff1
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-06 15:48:24 +0200

    Added 2 separate arrays and boolean variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,10 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({
   data() {
     return {
-      messagePlaceholder: 'Placeholder, Vue working!'
+      messagePlaceholder: 'Placeholder, Vue working!',
+      todoItems: [],
+      completedItems: [],
+      checkItemCompleted: false,
     }
   }
 }).mount('#app')
