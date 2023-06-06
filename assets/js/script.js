fc62f6c4ac033bf9ba73f0fd8b78dba245b16bed
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-06 15:49:27 +0200

    Added methods section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
 createApp({
-  data() {
-    return {
-      messagePlaceholder: 'Placeholder, Vue working!',
-      todoItems: [],
-      completedItems: [],
-      checkItemCompleted: false,
+    data() {
+        return {
+          messagePlaceholder: 'Placeholder, Vue working!',
+          todoItems: [],
+          completedItems: [],
+          checkItemCompleted: false,
+        }
+    },
+    methods: {
+    
     }
-  }
 }).mount('#app')
